b50c8cf510b55ea48a8340c2b0c4bff5e025f0ae
daniel130300 <daniel130300@outlook.com>
2024-12-07 00:37:55 -0600

    added service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
-
-const manifestForPlugIn = {
-  registerType:'prompt',
-  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
-  manifest:{
-    name: "Stock Tracker App",
-    short_name:"Stock App",
-    description:"I am a simple vite app",
-    icons:[{
-      src: '/android-chrome-192x192.png',
-      sizes:'192x192',
-      type:'image/png',
-      purpose:'favicon'
-    },
-    {
-      src:'/android-chrome-512x512.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'favicon'
-    },
-    {
-      src: '/apple-touch-icon.png',
-      sizes:'180x180',
-      type:'image/png',
-      purpose:'apple touch icon',
-    },
-    {
-      src: '/android-chrome-512x512.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'any maskable',
-    }
-  ],
-  theme_color:'#171717',
-  background_color:'#f0e7db',
-  display:"standalone",
-  scope:'/',
-  start_url:"/",
-  orientation:'portrait'
-  }
-} as Partial<VitePWAOptions>
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn)],
+  plugins: [
+    react(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.js',
+      manifest: {
+        name: 'Stock Tracker App',
+        short_name: 'StockTracker',
+        description: 'A Progressive Web App using Vite and React',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes:'192x192',
+            type:'image/png',
+            purpose:'favicon'
+          },
+          {
+            src:'/android-chrome-512x512.png',
+            sizes:'512x512',
+            type:'image/png',
+            purpose:'favicon'
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes:'180x180',
+            type:'image/png',
+            purpose:'apple touch icon',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
